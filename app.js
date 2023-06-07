52b60eba1766efca4f4d45e48c4a7d248afeb7ca
Kraeva Kate <kraevae53@gmail.com>
2023-06-07 14:44:22 +0300

    end of creating desing and adding of some js options for buttons

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 let color = document.querySelectorAll(".color");
+let sections = document.querySelectorAll("section");
 let buttonContinue = document.querySelector(".end button");
 let imgContinue = document.querySelector(".end img");
+let nextButtons = document.querySelectorAll(".next");
+let returnButton = document.querySelector(".return");
+let music = document.querySelectorAll(".music");
+let stopBut = document.querySelector(".stop");
+let mainPage = document.querySelector(".mainPage");
+let chosenColor = "";
+let chosenMusic = "";
 let colorList = [
   "rgb(251, 253, 110)",
   "rgb(199, 3, 78)",
@@ -13,15 +21,15 @@ let colorList = [
   "rgb(92, 240, 72)",
   "rgb(255, 97, 144)",
   "rgb(253, 125, 75)",
-  "rgb(210, 116, 247)",
+  "linear-gradient(90deg, rgba(255,148,177,0.9920343137254902) 17%, rgba(208,92,243,1) 61%)",
 ];
 color.forEach((el, index) => {
   el.style.background = colorList[index];
   el.addEventListener("click", () => {
     color.forEach((c) => {
-      c.style.border = "1px solid white";
+      c.classList.remove("choose");
     });
-    el.style.border = "3px solid white";
+    el.classList.add("choose");
   });
 });
 buttonContinue.addEventListener("mouseover", () => {
@@ -29,4 +37,44 @@ buttonContinue.addEventListener("mouseover", () => {
 });
 buttonContinue.addEventListener("mouseout", () => {
   imgContinue.src = "image/again.png";
+});
+nextButtons.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    sections[i].style.marginTop = "-100vh";
+  });
+});
+returnButton.addEventListener("click", () => {
+  document.querySelector(".end").style.marginTop = "-100vh";
+  document.querySelector(".optionContMain").style.marginTop = "0px";
+});
+let last = document.querySelector(".last");
+let lastColorP = document.querySelector(".last p");
+last.addEventListener("mouseover", () => {
+  lastColorP.style.display = "block";
+});
+last.addEventListener("mouseout", () => {
+  lastColorP.style.display = "none";
+});
+music.forEach((m) => {
+  m.addEventListener("click", () => {
+    music.forEach((mus) => {
+      mus.classList.remove("chooseMusic");
+    });
+    m.classList.add("chooseMusic");
+  });
+});
+document.querySelector(".startBut").addEventListener("click", () => {
+  chosenColor = document.querySelector(".choose").style.background;
+  chosenMusic = document.querySelector(".chooseMusic");
+});
+window.addEventListener("load", () => {
+  sections.forEach((s) => {
+    s.style.display = "none";
+  });
+  sections[0].style.display = 'block'
+});
+mainPage.addEventListener("click", () => {
+  sections.forEach((s) => {
+    s.style.display = "flex";
+  });
 });
